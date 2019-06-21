5e90c1ec4e34dcb99601eef2d45a8ea6fc82a8d9
Sebi <sebi@tuximail.de>
2019-06-21 22:59:38 +0200

    added script for rss feed generation

---
+++ feedgenerator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo "please pass the path of your html files as first argument" && exit 1
+
+cd "$1"
+
+posts=$(cat index.html | grep -i "<li>" | sed 's/.*">//' | sed 's/<.*//')
+
+echo "<?xml version='1.0' encoding='utf-8'?>
+<rss version='2.0'>
+<channel>
+<title>Tuximail</title>
+<description>A Linux Blog with irregular updates</description>
+<link>https://tuximail.github.io</link>
+<language>en-us</language>
+<pubDate>$(date)</pubDate>" > tuximail.rss
+
+
+while read -r title; do
+	line=$(cat index.html | grep "$title")
+	date=$(echo "$line" | sed 's/<li>//' | sed 's/-.*//')
+	url=$(echo "$line" | sed 's/.*="//' | sed 's/">.*//')
+	content=$(cat "$url")
+	echo "<item>
+		<title>${title}</title>
+		<description><![CDATA[${content}]]></description>
+		<link>https://tuximail.github.io/${url}</link>
+		<guid isPermalink="false">${url}</guid>
+		<pubDate>${date}</pubDate>
+	</item>" >> tuximail.rss
+
+
+done <<< "$posts"
+
+echo "</channel>
+</rss>" >> tuximail.rss
+
